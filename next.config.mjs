1e53f92eb876e0f599a874d13e34d3d2219eb72a
MIN-CHUL <bbb1sss2@nate.com>
2026-06-03 17:22:23 +0900

    new_detail

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['image.coupangcdn.com', 'thumbnail7.coupangcdn.com', 'thumbnail6.coupangcdn.com'],
+    domains: [
+      'image.coupangcdn.com',
+      'thumbnail.coupangcdn.com',
+      'thumbnail6.coupangcdn.com',
+      'thumbnail7.coupangcdn.com',
+    ],
   },
 };
 
