81c8f3b47150beb3bf9b5514935bec62f942053a
minchul <bbb1sss3@gmail.com>
2026-06-08 08:26:43 +0900

    update2

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,17 +31,18 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KZGRD2JK"
-            height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
-        </noscript>
         {children}
-        <Script id="gtm" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-KZGRD2JK');`}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-0ZH4DXB4E6"
+          strategy="afterInteractive"
+        />
+        <Script id="ga4" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0ZH4DXB4E6');
+          `}
         </Script>
       </body>
     </html>
