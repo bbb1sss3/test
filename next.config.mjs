49271d06118c9683cbc2451ae61eef222546e4bb
minchul <bbb1sss3@gmail.com>
2026-07-02 15:06:21 +0900

    update_ihreb

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       { protocol: 'https', hostname: 'image.coupangcdn.com' },
       { protocol: 'https', hostname: 'thumbnail.coupangcdn.com' },
